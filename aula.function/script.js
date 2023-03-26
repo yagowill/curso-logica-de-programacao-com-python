932470f8b09dfbf590d93cca445e8a57044c2b17
yagowill <yagowill3@gmail.com>
2023-03-26 00:04:23 -0300

    funções

---
+++ aula.function/script.js
@@ -0,0 +1,27 @@
+// Verbo + Substantivo
+// let conta = 150;
+// function pagarConta(valor){
+//     conta = valor;
+// }
+
+// console.log(conta)
+// pagarConta(0);
+// console.log(conta);
+
+// Tipos de função
+//Realiza uma tarefa, não devolve nada
+function dizerNome(){
+    console.log('yago')
+};
+dizerNome();
+// Faz um cálculo ou operação e retorna algo
+function MultuplicarPorDois(valor){
+    return valor* 2;
+};
+
+console.log(MultuplicarPorDois(5));
+
+let resultado = MultuplicarPorDois(5);
+
+console.log(resultado);
+
