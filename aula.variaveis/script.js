c1ca7e16ca0312c4058f8c353943198a8a03b395
yagowill <yagowill3@gmail.com>
2023-03-26 00:06:05 -0300

    variaveis

---
+++ aula.variaveis/script.js
@@ -0,0 +1,29 @@
+// let dia = 19;
+// let mes = 01;
+// let ano = 2000;
+// console.log('Data de nascimento:')
+// console.log(dia, mes, ano);
+
+// let dia, mes, ano;
+// dia = 19;
+// mes = 01;
+// ano = 2000;
+// console.log('Data de nascimento', dia, mes, ano)
+
+// let nome = 'Rafael'; //string literal
+// let idade = 25; //number literal
+// let estaAprovado = true; //boolena
+// let sobrenome ; // Undefined sem valor nenhum
+// let corSelecionada = null; //null redefinir
+
+//  let pessoa = {
+//  nome: 'Yago',
+//  idade: 22,
+//  estaAprovado: true,
+//  sobrenome: 'martins',
+//  faculdade: 'ciências da computação',
+//  hobby: 'tocar guitarra',
+//  trabalhando: true
+// };
+
+//  console.log(pessoa);
